7bd079edc48eed177016df3e9d8287fb53eec561
Andrew H. Jaffe <a.h.jaffe@gmail.com>
2018-03-20 11:12:32 +0000

    move to package version

---
+++ pip_upgrade-runner.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+ 
+"""Convenience wrapper for running pushover directly from source tree.""" 
+ 
+from pip_upgrade.pip_upgrade import main
+
+if __name__ == '__main__':
+    main()
+    
--- pip_upgrade_outdated/__main__.py
+++ pip_upgrade_outdated/__main__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
  
  
-"""pip_upgrade.__main__: executed when pushover directory is called as script."""
+"""pip_upgrade_outdated.__main__: executed when pushover directory is called as script."""
  
  
 from .upgrade_pip_packages import main
